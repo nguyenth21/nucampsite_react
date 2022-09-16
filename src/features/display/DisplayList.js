9d72d574498f9ab0a2de4bef35f9648e851f128b
Thanh Nguyen <nguyenth21@gmail.com>
2022-09-15 20:41:28 -0500

    Async DisplayList

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -5,23 +5,32 @@ import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 
 const DisplayList = () => {
   const items = useSelector((state) => [
     selectFeaturedCampsite(state),
     selectFeaturedPromotion(state),
-    selectFeaturedPartner(state)
+    selectFeaturedPartner(state),
   ]);
-  
-  console.log('display items:', items);
+
+  console.log("display items:", items);
 
   return (
     <Row>
       {items.map((item, idx) => {
+        const { featuredItem, isLoading, errMsg } = item;
+        if (isLoading) {
+          return <Loading key={idx} />;
+        }
+        if (errMsg) {
+          return <Error errMsg={errMsg} key={idx} />;
+        }
         return (
-          item && (
+          featuredItem && (
             <Col md className="m-1" key={idx}>
-              <AnimatedDisplayCard item={item} />
+              <AnimatedDisplayCard item={featuredItem} />
             </Col>
           )
         );
